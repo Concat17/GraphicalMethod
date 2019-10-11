17453ea897061cd1fe3f38d81432604e273c3392
Demel654 <Demel654@gmail.com>
2019-10-12 02:10:29 +0300

    Correct fill area

--- GraphicalMethod/Form1.cs
+++ GraphicalMethod/Form1.cs
@@ -2,6 +2,7 @@
 using System.Windows.Forms;
 using System.Linq;
 using System;
+using System.Collections.Generic;
 
 /*
  * 1 Interface
@@ -10,6 +11,9 @@ using System;
  * 4 Points Finder
  * 5 Filter
  * 6 Polygon drawing
+ * 7 Fix Stack overflow at examples like this x <= 4
+ * 8 Add Z-func and vector n
+ * 9 Implement finding max and min
  */
 namespace GraphicalMethod
 {
@@ -36,15 +40,18 @@ namespace GraphicalMethod
 
         private void BtnRes_Click(object sender, System.EventArgs e)
         {
-            Line line1 = new Line(3, 2, 12, false);
-            line1.Draw(g, Pens.Red);
+            //Line line1 = new Line(3, 2, 12, false);
+            //line1.Draw(g, Pens.Red);
 
-            Line line2 = new Line(1, 2, 4, true);//find points filter points
-            line2.Draw(g, Pens.Green);
+            //Line line2 = new Line(1, 2, 4, true);//find points filter points
+            //line2.Draw(g, Pens.Green);
 
-            Line line3 = new Line(2, -1, 1, false);
-            line3.Draw(g, Pens.Blue);
+            //Line line3 = new Line(2, -1, 1, false);
+            //line3.Draw(g, Pens.Blue);
 
+            //var testPoints = new List<PointF>() { new PointF(1, 2), new PointF(3, 2), new PointF(2, 1), new PointF(2, 3) };
+            //var res = testPoints.OrderBy(x => Math.Atan2(x.X, x.Y)).ToList();
+            //var f = 3;
             //PointManipulator.isLand(line3, new PointF(1.2f, 1.4f));
 
             //Line line1 = new Line(-1, 1, 5, false);
@@ -63,13 +70,21 @@ namespace GraphicalMethod
             //line2.Draw(g, Pens.Red);
 
             //Line line3 = new Line(-2, 1, -2, false);
-            //line3.Draw(g, Pens.Green);
+            //line3.Draw(g, Pens.Green); 
 
+            Line line1 = new Line(2, 4, 20, false);
+            line1.Draw(g, Pens.Black);
+
+            Line line2 = new Line(3, -5, 1, false);//find points filter points
+            line2.Draw(g, Pens.Red);
+
+            Line line3 = new Line(1, 0, 4, false);
+            line3.Draw(g, Pens.Green);
 
             //var points = PointManipulator.FilterPoints(line1, line2, line3);
             //var scaledPoints = points.Select(p => Line.ScalePoint(p)).ToList();
             var points = PointManipulator.getFigurePoints(line1, line2, line3, panel1.Size);
-            points = points.OrderBy(x => Math.Atan2(x.X, x.Y)).ToList();
+           
             foreach (var point in points)
             {
                 g.DrawLine(Pens.Pink, point, new PointF(300, 300));
--- GraphicalMethod/PointManipulator.cs
+++ GraphicalMethod/PointManipulator.cs
@@ -15,7 +15,15 @@ namespace GraphicalMethod
         public static List<PointF> getFigurePoints(Line l1, Line l2, Line l3, Size s)
         {
             var points = FilterPoints(l1, l2, l3);
-            var scaledPoints = points.Select(p => Line.ScalePoint(p)).ToList();
+            //points.Insert(0, points[2]);
+            ////points.RemoveAt(3);
+            //points.Insert(0, points[3]);
+            //points.RemoveAt(4);
+            //points = points.Select(p => new PointF((float)Math.Round(p.X), (float)Math.Round(p.Y))).ToList(); 
+            //var orderedPoints = points.OrderBy(x => Math.Atan2(x.X, x.Y)).ToList();
+            var orderedPoints = SortPoints(points);
+            
+            var scaledPoints = orderedPoints.Select(p => Line.ScalePoint(p)).ToList();
             var res = scaledPoints.Where(p => p.X >= s.Width / 2 && p.Y <= s.Height / 2).ToList();
             return res;
         }
@@ -59,7 +67,7 @@ namespace GraphicalMethod
             var filteredPoints = new List<PointF>();
             foreach(var point in points)
             {
-                if (isLand(l1, point) && isLand(l2, point) && isLand(l3, point))
+                if (isFirstQuarter(point) && isLand(l1, point) && isLand(l2, point) && isLand(l3, point))
                     filteredPoints.Add(point);
             }
             return filteredPoints;
@@ -73,6 +81,67 @@ namespace GraphicalMethod
                 return true;
             return false;
         }
-         
+
+        public static bool isFirstQuarter(PointF p)
+        {
+            return p.X >= 0 && p.Y >= 0;
+        }
+
+        static List<PointF> SortPoints(List<PointF> points)
+        {
+            var cPoints = new List<PointF>(points);
+            var sorted = new List<PointF>();
+            sorted.Add(cPoints[cPoints.Count - 1]);
+            cPoints.RemoveAt(cPoints.Count - 1);
+            while (cPoints.Count > 0)
+            {
+                var indClosest = FindClosest(sorted[sorted.Count - 1], cPoints);
+                sorted.Add(cPoints[indClosest]);
+                cPoints.RemoveAt(indClosest);
+            }
+            return sorted;
+        }
+
+        private static int FindClosest(PointF lastP, List<PointF> cPoints)
+        {
+            var ind = 0;
+            for (var i = 0; i < cPoints.Count; i++)
+            {
+                if (isLayInOneLine(lastP, cPoints[i], cPoints))
+                    return i;
+            }
+            return ind;
+        }
+
+        private static bool isLayInOneLine(PointF lp1, PointF lp2, List<PointF> cPoints)
+        {
+            var checker = 0;
+            foreach (var p in cPoints)
+            {
+                if (lp2 == p)
+                    continue;
+                if (checker == 0)
+                {
+                    checker = isAbove(lp1, lp2, p);
+                }
+                else
+                {
+                    if (checker != isAbove(lp1, lp2, p))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private static int isAbove(PointF lp1, PointF lp2, PointF p)
+        {
+            if ((p.X - lp1.X) / (lp2.X - lp1.X) - (p.Y - lp1.Y) / (lp2.Y - lp1.Y) > 0)
+                return 2;
+            return 1;
+        }
+
+
     } 
 }
